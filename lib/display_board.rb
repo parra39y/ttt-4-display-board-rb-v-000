247255a7c5de4d3832a310fe6a5423eb066ad01c
Yesenia Parra <parra22y@mtholyoke.edu>
2018-03-24 22:59:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,8 +8,6 @@ puts "-----------"
 puts "   |   |   "
 end
 
-def display_board (board = [" "," "," "," "," "," "," "," "," "])
-  puts "O:#{board[0]}, X:#{board[1]},<code>"&nbsp;&nbsp;&nbsp;"</code>"
-  puts "<code>"&nbsp;&nbsp;&nbsp;"</code>,X:#{board[4]},<code>"&nbsp;&nbsp;&nbsp;"</code>"
-  Puts " X:#{board[6]}, O:#{board[7]}<code>"&nbsp;&nbsp;&nbsp;"</code>"
+def display_board (board = [" "," "," "," "," "," "," "," "," "]
+  puts "O:#{board[0]}, X:#{board[1]}, X:#{board[4]}, X:#{board[6]}, O:#{board[7]}"
 end
